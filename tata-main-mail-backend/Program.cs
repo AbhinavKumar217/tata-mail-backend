6ac37eb6f9b269e2a03022ae6b505b1a95cb5f5a
AbhinavKumar217 <88775045+AbhinavKumar217@users.noreply.github.com>
2023-07-22 23:10:15 +0530

    Add files via upload

---
+++ tata-main-mail-backend/Program.cs
@@ -0,0 +1,35 @@
+global using tata_main_mail_backend.Services.EmailService;
+global using tata_main_mail_backend.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IEmailService, EmailService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(builder => builder
+    .WithOrigins("http://localhost:3000")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials());
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ tata-main-mail-backend/Services/EmailService/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace tata_main_mail_backend.Services.EmailService
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailDTO request);
+    }
+}
